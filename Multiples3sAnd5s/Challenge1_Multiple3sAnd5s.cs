f3e0186f970e40f0e9694f097e0113cacafcb0c7
Blake Harrison <Blakemharrison@outlook.com>
2023-07-28 22:03:17 -0500

    Made 3 & 5

---
+++ Multiples3sAnd5s/Challenge1_Multiple3sAnd5s.cs
@@ -0,0 +1,20 @@
+﻿namespace Multiples3sAnd5s
+{
+    internal class Challenge1_Multiple3sAnd5s
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 0; 3 * i < 1000; i++)
+            {
+                sum += 3 * i;
+
+                if (5 * i < 1000 && (5 * i) % 3 != 0)
+                {
+                    sum += 5 * i;
+                }
+            }
+            Console.WriteLine($"Multiples of 3 and 5 out of 1000 added together is {sum}.");
+        }
+    }
+}
